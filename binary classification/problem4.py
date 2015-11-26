63b2f584caf14059caced7d49fcb036c480968ab
Verna Koskinen <darth.kipsu@gmail.com>
2015-11-26 09:22:13 +0200

    shorten names for variables

--- binary classification/problem4.py
+++ binary classification/problem4.py
@@ -1,4 +1,6 @@
+from scipy.spatial.distance import cdist
 from sklearn.neighbors import KNeighborsClassifier
+
 import math
 import matplotlib.pyplot as plt
 import numpy as np
@@ -19,22 +21,22 @@ def draw_datapoints(labels):
 Generate 10k samples, classify with the boundary above and compute error rate.
 '''
 boundary_labels = np.random.randint(2, size=10000)
-boundary_x1 = draw_datapoints(boundary_labels)
-boundary_x2 = draw_datapoints(boundary_labels)
-boundary_errors_y0 = boundary_x1[(boundary_labels==0) & (boundary_x1 * boundary_x1 + boundary_x2 * boundary_x2 > math.pow(boundary, 2))]
-boundary_errors_y1 = boundary_x1[(boundary_labels==1) & (boundary_x1 * boundary_x1 + boundary_x2 * boundary_x2 <= math.pow(boundary, 2))]
-boundary_errors = (len(boundary_errors_y0) + len(boundary_errors_y1)) / 10000.0
+b_x1 = draw_datapoints(boundary_labels)
+b_x2 = draw_datapoints(boundary_labels)
+b_errors_y0 = b_x1[(boundary_labels==0) & (b_x1 * b_x1 + b_x2 * b_x2 > math.pow(boundary, 2))]
+b_errors_y1 = b_x1[(boundary_labels==1) & (b_x1 * b_x1 + b_x2 * b_x2 <= math.pow(boundary, 2))]
+boundary_errors = (len(b_errors_y0) + len(b_errors_y1)) / 10000.0
 
 '''
 Create training set with 500 items and print a scatterplot of them
 '''
 training_labels = np.random.randint(2, size=500)
-training_x1 = draw_datapoints(training_labels)
-training_x2 = draw_datapoints(training_labels)
-training_data = np.column_stack((training_x1, training_x2))
+train_x1 = draw_datapoints(training_labels)
+train_x2 = draw_datapoints(training_labels)
+training_data = np.column_stack((train_x1, train_x2))
 
-plt.scatter(training_x1[training_labels == 1], training_x2[training_labels == 1], color='blue', edgecolor='black')
-plt.scatter(training_x1[training_labels == 0], training_x2[training_labels == 0], color='red', edgecolor='black')
+plt.scatter(train_x1[training_labels == 1], train_x2[training_labels == 1], color='blue', edgecolor='black')
+plt.scatter(train_x1[training_labels == 0], train_x2[training_labels == 0], color='red', edgecolor='black')
 plt.title('Training set')
 plt.show()
 
@@ -60,6 +62,21 @@ def kNN_missclassifications(k, data, labels):
     predictions = neigh.predict(data)
     errors = predictions[predictions != labels]
     return len(errors) / float(len(labels))
+
+distances = cdist(test_data, training_data)
+
+def kNN_predictions(k):
+    return training_labels[np.argmin(distances, 1)] # this does not take k into account currently
+
+#print cdist(test_data, training_data)
+print kNN_predictions(1)
+#print kNN_predictions(15)
+
+def kNN_missclassifications_my(k, data, labels):
+    predictions = kNN_predictions(k)
+    return predictions[predictions != labels]
+
+print kNN_missclassifications(1, test_data, test_labels)
 
 k_values = [57, 41, 33, 25, 21, 17, 13, 9, 7, 5, 3, 1]
 
